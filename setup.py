bdee567a19b895da006c039c7db562e079a1e890
Vivaan Verma <54081925+doublevcodes@users.noreply.github.com>
2021-08-03 23:32:16 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="scheduguru",
-    version="0.0.1",
+    version="0.0.3",
     description="Schedule Python functions with ease",
     long_description=README,
     long_description_content_type="text/markdown",
